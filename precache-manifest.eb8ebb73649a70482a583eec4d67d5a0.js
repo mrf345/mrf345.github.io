c1c4167ff08e205aa8ce91e555fbde9e42d0b078
GitHub Action <action@github.com>
2020-07-02 19:16:25 +0000

    Deploy build files from testing

---
+++ precache-manifest.eb8ebb73649a70482a583eec4d67d5a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64de07c09d1b22916edfa15f50c689d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93b52427c8e8ee66eb9a",
+    "url": "/static/css/main.893ed83d.chunk.css"
+  },
+  {
+    "revision": "004915553923f96b99f8",
+    "url": "/static/js/2.4a7ad824.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/static/js/2.4a7ad824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93b52427c8e8ee66eb9a",
+    "url": "/static/js/main.f8259b83.chunk.js"
+  },
+  {
+    "revision": "f93161848696b3e015c5",
+    "url": "/static/js/runtime-main.ef475df3.js"
+  }
+]);
