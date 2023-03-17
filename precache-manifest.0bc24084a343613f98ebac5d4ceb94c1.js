11be7d4900e6a246ffc5446fbb35990799b3ee3c
GitHub Action <action@github.com>
2023-03-17 04:29:40 +0000

    Deploy build files from testing

---
+++ precache-manifest.0bc24084a343613f98ebac5d4ceb94c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34073fa67be0d59163038be9e4a9b9fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "462819310e8460e3786c",
+    "url": "/static/css/main.3bcf2045.chunk.css"
+  },
+  {
+    "revision": "ffcca3b4a3444c4820e2",
+    "url": "/static/js/2.17324327.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/static/js/2.17324327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "462819310e8460e3786c",
+    "url": "/static/js/main.4906d20b.chunk.js"
+  },
+  {
+    "revision": "f93161848696b3e015c5",
+    "url": "/static/js/runtime-main.ef475df3.js"
+  }
+]);
