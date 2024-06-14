637871cb6f9ccb949919b2eea84c3561ec36e2ae
GitHub Action <action@github.com>
2024-06-14 08:54:33 +0000

    Deploy build files from testing

---
+++ precache-manifest.3996349fccd8714936661fd8698e53ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbdf31e6548f9e1f8b911dcc3b223ddd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2743016927af2dc3384",
+    "url": "/static/css/main.3bcf2045.chunk.css"
+  },
+  {
+    "revision": "ffcca3b4a3444c4820e2",
+    "url": "/static/js/2.17324327.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/static/js/2.17324327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2743016927af2dc3384",
+    "url": "/static/js/main.5ddb1e8e.chunk.js"
+  },
+  {
+    "revision": "f93161848696b3e015c5",
+    "url": "/static/js/runtime-main.ef475df3.js"
+  }
+]);
--- src/globas.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react'
-
-// Disable or enable animation
-export const AnimationContext = createContext(true)
-
-
-// Duration to delay execution for in milliseconds
-// increased to give a processing effect
-export const LOOP_DELAY = 1500
